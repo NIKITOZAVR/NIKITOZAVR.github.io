555adca5f6c7b8fd632afd5d641334efe454a851
NIKITOZAVR <nikitozavr@email.ua>
2018-06-24 23:41:26 +0300

    1

---
+++ blueair/js/script.js
@@ -0,0 +1,23 @@
+$(function(){
+
+
+  $(".menubtn480").click(function(){
+
+     $(".menu-480").slideToggle();
+     $(this).toggleClass("a-menubtn480");
+
+    //
+
+
+  });
+
+
+    // function(){
+    //     $('.menu-480').css('display','block');
+    // },
+    // function(){
+    //     $('.menu-480').css('display','none');
+
+
+
+});
