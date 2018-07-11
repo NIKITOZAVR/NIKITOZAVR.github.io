d04da1cec718e194d84062445f0c424981e619ce
NIKITOZAVR <nikitozavr@email.ua>
2018-07-11 13:45:02 +0300

    1

---
+++ kush/js/script.js
@@ -0,0 +1,12 @@
+$(function(){
+
+
+  $('#hamburger')[0].onclick = function () {
+    // $('.wrapper').toggleClass('blur');
+    $('.menu').toggleClass('menu--active');
+    $('.we-offer').toggleClass('active');
+  }
+
+
+
+});
